fa8a546847e631569857c40ffba1a27f8af45fac
Emad Gelani <egelani@jahez.net>
2024-08-22 01:57:15 +0300

    add docker file

--- Playground.HLS/Controllers/VideosController.cs
+++ Playground.HLS/Controllers/VideosController.cs
@@ -21,7 +21,7 @@ namespace Playground.HLS.Controllers
         {
             var uploadedFile = request.File;
             var outputFolderPath = "videos";
-            var exePath = "C:\\ProgramData\\chocolatey\\lib\\ffmpeg\\tools\\ffmpeg\\bin";
+            var exePath = "/usr/bin";//"C:\\ProgramData\\chocolatey\\lib\\ffmpeg\\tools\\ffmpeg\\bin";
             FFmpeg.SetExecutablesPath(exePath);
             string baseDirectory = Path.Combine(_hostingEnvironment.ContentRootPath, "Videos");
 
@@ -31,7 +31,7 @@ namespace Playground.HLS.Controllers
 
             string tempInputFilePath = Path.Combine(baseDirectory, "TempVideos", Path.GetFileName(uploadedFile.FileName));
             string outputFilePath = Path.Combine(baseDirectory, Path.GetFileName(uploadedFile.FileName), "index.m3u8");
-          //  string outputFilePath = Path.Combine(baseDirectory, "DoneVideos", "index.m3u8");
+            //  string outputFilePath = Path.Combine(baseDirectory, "DoneVideos", "index.m3u8");
 
             try
             {
